cbc9bdb31fb67b6ae78c7e4393621e81aed43b88
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-05 20:29:59 +0530

    Store the siglet in Dictionary and calculate the percentage of singlet

--- flipcoinsimulation2.sh
+++ flipcoinsimulation2.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -x
 echo "flip coin simulation"
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+H=0
+T=0
+i=0
+declare -A dict
+while [[ $H -lt 50 || $T -lt 50 ]]
+do
+   randomCheck=$((RANDOM%2))
+   if [ $randomCheck -eq 1 ]
+   then
+      ((H++))
+   dict[ "$i" ]="H"
+   else
+      ((T++))
+      dict[ "$i" ]="T"
+   fi
+((i++))
+done
+totalNumberOfFliping=$i
+echo "totalNumberOfFlipingCoin: $totalNumberOfFliping Heads:$H Tails:$T "
+HeadsPercentage=$((($H*100)/$totalNumberOfFliping))
+echo "HeadsPercentage:$HeadsPercentage"
+TailsPercentage=$((($T*100)/$totalNumberOfFliping))
+echo "TailsPercentage:$TailsPercentage"
