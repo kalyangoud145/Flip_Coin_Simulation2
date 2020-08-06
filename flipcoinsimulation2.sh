59441b8413389d6b0337ce30e25d3b49feeeb3ec
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-06 12:59:19 +0530

    Triplet combination and percentage calculation

--- flipcoinsimulation2.sh
+++ flipcoinsimulation2.sh
@@ -1,5 +1,31 @@
 #!/bin/bash 
 echo "flip coin simulation"
+#Singlet combination
+H=0
+T=0
+i=0
+declare -A dict
+while [[ $H -lt 50 || $T -lt 50 ]]
+do
+   randomCheck=$((RANDOM%2))
+   if [ $randomCheck -eq 1 ]
+   then
+      ((H++))
+   dict[ "$i" ]="H"
+   else
+      ((T++))
+      dict[ "$i" ]="T"
+   fi
+((i++))
+done
+totalNumberOfFliping=$i
+echo "totalNumberOfFlipingCoin: $totalNumberOfFliping Heads:$H Tails:$T "
+HeadsPercentage=$((($H*100)/$totalNumberOfFliping))
+echo "HeadsPercentage:$HeadsPercentage"
+TailsPercentage=$((($T*100)/$totalNumberOfFliping))
+echo "TailsPercentage:$TailsPercentage"
+echo "${dict[@]}"
+#Doublet combination
 HH=0
 HT=0
 TH=0
@@ -29,7 +55,6 @@ do
 ((j++))
 ((double++))
 done
-
 totalNumberOfFliping2=$j
 echo "totalNumberOfFlipingCoin: $totalNumberOfFliping2 HH:$HH HT:$HT TH:$TH TT:$TT "
 HHPercentage=$((($HH*100)/$totalNumberOfFliping2))
@@ -41,3 +66,75 @@ echo "THPercentage:$THPercentage"
 TTPercentage=$((($TT*100)/$totalNumberOfFliping2))
 echo "TTPercentage:$TTPercentage"
 echo "${dict1[@]}"
+
+#Triplet combination
+HHH=0
+HHT=0
+HTH=0
+THH=0
+HTT=0
+THT=0
+TTH=0
+TTT=0
+k=0
+third=0
+declare -A dict2
+while [ $third -lt 200 ]
+do
+   randomCheck=$((RANDOM%8))
+   if [ $randomCheck -eq 0 ]
+   then
+      ((HHH++))
+   	dict2[ "$k" ]="HHH"
+   elif [ $randomCheck -eq 1 ]
+   then
+      ((HHT++))
+      dict2[ "$k" ]="HHT"
+   elif [ $randomCheck -eq 2 ]
+   then
+      ((HTH++))
+       dict2[ "$k" ]="HTH"
+   elif [ $randomCheck -eq 3 ]
+	then
+      ((THH++))
+       dict2[ "$k" ]="THH"
+	elif [ $randomCheck -eq 4 ]
+   then
+      ((HTT++))
+       dict2[ "$k" ]="HHT"
+	elif [ $randomCheck -eq 5 ]
+   then
+      ((THT++))
+       dict2[ "$k" ]="THT"
+	elif [ $randomCheck -eq 6 ]
+   then
+      ((TTH++))
+       dict2[ "$k" ]="TTH"
+	else
+		((TTT++))
+	 dict2[ "$k" ]="TTT"
+   fi
+((k++))
+((third++))
+done
+totalNumberOfFliping3=${#dict2[@]}
+echo "totalNumberOfFlipingCoin: $totalNumberOfFliping3 HHH:$HHH HHT:$HHT HTH:$HTH THH:$THH HTT:$HTT THT:$THT TTH:$TTH TTT:$TTT "
+HHHPercentage=$((($HHH*100)/$totalNumberOfFliping3))
+echo "HHHPercentage:$HHHPercentage"
+HHTPercentage=$((($HHT*100)/$totalNumberOfFliping3))
+echo "HHTPercentage:$HHTPercentage"
+HTHPercentage=$((($HTH*100)/$totalNumberOfFliping3))
+echo "HTHPercentage:$HTHPercentage"
+THHPercentage=$((($THH*100)/$totalNumberOfFliping3))
+echo "THHPercentage:$THHPercentage"
+HTTPercentage=$((($HTT*100)/$totalNumberOfFliping3))
+echo "HTTPercentage:$HTTPercentage"
+THTPercentage=$((($THT*100)/$totalNumberOfFliping3))
+echo "THTPercentage:$THTPercentage"
+THTPercentage=$((($THT*100)/$totalNumberOfFliping3))
+echo "THTPercentage:$THTPercentage"
+TTHPercentage=$((($TTH*100)/$totalNumberOfFliping3))
+echo "TTHPercentage:$TTHPercentage"
+TTTPercentage=$((($TTT*100)/$totalNumberOfFliping3))
+echo "TTTPercentage:$TTTPercentage"
+echo "${dict2[@]}"
